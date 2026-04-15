210ba7ec483115d923eccdfc87112a902a1801e6
Nethra Viswanathan <60757925+nethra8902@users.noreply.github.com>
2026-04-15 22:13:03 +0100

    Add AssemblyInfo.cs for internal visibility to tests

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("UrlSnip.Tests")]
+// If the main assembly is signed, use the fully qualified name with the public key:
+// [assembly: InternalsVisibleTo("UrlSnip.Api.Tests, PublicKey=002400000480000094...")]
